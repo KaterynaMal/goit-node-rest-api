51ec90b8016429754636c7023d947b83e4dd3ca7
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2024-03-08 16:27:04 +0200

    model, routes, controller, sercices

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -4,10 +4,12 @@ export const createContactSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.string().required(),
+  favorite: Joi.boolean().required(),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().regex(/[a-zA-Z]+/),
   email: Joi.string().email(),
   phone: Joi.string().regex(/^[+0-9()\-.\s]+$/),
+  favorite: Joi.boolean(),
 });
--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,72 +1,15 @@
-import fs from "fs/promises";
-import path from "path";
-import { nanoid } from "nanoid";
+import Contact from "../models/Contact.js";
 
-const contactsPath = path.resolve("db", "contacts.json");
+export const listContacts = () => Contact.find();
 
-export async function listContacts() {
-  try {
-    const data = await fs.readFile(contactsPath, "utf-8");
-    return JSON.parse(data);
-  } catch (error) {
-    console.error("Error reading contacts file:", error);
-    return [];
-  }
-}
+export const addContact = data => Contact.create(data);
 
-export async function getContactById(contactId) {
-  try {
-    const contacts = await listContacts();
-    const result = contacts.find((contact) => contact.id === contactId);
-    return result || null;
-  } catch (error) {
-    console.error("Error getting contact by id:", error);
-    return null;
-  }
-}
+export const getContactById = id => Contact.findById(id);
 
-export async function removeContact(contactId) {
-  try {
-    const contacts = await listContacts();
-    const index = contacts.findIndex((contact) => contact.id === contactId);
-    if (index === -1) {
-      return null;
-    }
-    const [result] = contacts.splice(index, 1);
+export const removeContact = id => Contact.findByIdAndDelete(id);
 
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return result;
-  } catch (error) {
-    console.error("Error removing contact:", error);
-    throw error;
-  }
-}
+export const updateContact = (id, data) => Contact.findByIdAndUpdate(id, data, {new: true, runValidators: true});
 
-export async function addContact(name, email, phone) {
-  try {
-    const contacts = await listContacts();
-    const newContact = {
-      id: nanoid(),
-      name,
-      email,
-      phone,
-    };
-    contacts.push(newContact);
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return newContact;
-  } catch (error) {
-    console.error("Error adding contact:", error);
-    return null;
-  }
-}
+export const updateStatusContact = (id, updateData) => Contact.findByIdAndUpdate(id, updateData, {new: true});
 
-export async function updateContact(id, data) {
-  const contacts = await listContacts();
-  const index = contacts.findIndex((contact) => contact.id === id);
-  if (index === -1) {
-    return null;
-  }
-  contacts[index] = { ...contacts[index], ...data };
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return contacts[index];
-}
+
